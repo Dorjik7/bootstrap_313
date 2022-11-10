bbc2458a0eb87168bf997aa9b2373cfeb65dbd99
Maria Dryukova <mdryukova@yandex.ru>
2022-11-10 23:38:13 +0300

    Новый класс InitiateDB (c пользователями admin-admin, user-user)

---
+++ src/main/java/ru/dorjik/bootstrap_313/service/InitiateDB.java
@@ -0,0 +1,22 @@
+package ru.dorjik.bootstrap_313.service;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import ru.dorjik.bootstrap_313.models.User;
+import java.util.Collections;
+
+@Component
+public class InitiateDB implements ApplicationRunner {
+    private final UserService usersService;
+
+    public InitiateDB(UserService usersService) {
+        this.usersService = usersService;
+    }
+    @Override
+    public void run(ApplicationArguments args){
+        User admin = new User( "admin", "admin", "admin", "admin",22, "admin@mail.ru", Collections.singleton("ROLE_ADMIN"));
+        User user = new User( "user", "user", "user", "user",21, "user@mail.ru", Collections.singleton("ROLE_USER"));
+        usersService.addUser(admin);
+        usersService.addUser(user);
+    }
+}
