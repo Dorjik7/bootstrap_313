7d660782202ce9d414e608d4467a36d6396ec5e7
Maria Dryukova <mdryukova@yandex.ru>
2022-11-10 23:24:52 +0300

    Добавила класс InitiateDB с пользователями (admin-admin, user-user)

--- src/main/java/ru/dorjik/bootstrap_313/service/InitiateDB.java
+++ src/main/java/ru/dorjik/bootstrap_313/service/InitiateDB.java
@@ -16,8 +16,8 @@ public class InitiateDB implements ApplicationRunner {
     }
     @Override
     public void run(ApplicationArguments args){
-        User admin = new User( "admin", "admin", "admin", "admin",26, "admin@mail", Collections.singleton("ROLE_ADMIN"));
-        User user = new User( "user", "user", "user", "user",26, "admin@mail", Collections.singleton("ROLE_USER"));
+        User admin = new User( "admin", "admin", "admin", "admin",22, "admin@mail.ru", Collections.singleton("ROLE_ADMIN"));
+        User user = new User( "user", "user", "user", "user",21, "user@mail.ru", Collections.singleton("ROLE_USER"));
         usersService.addUser(admin);
         usersService.addUser(user);
     }
